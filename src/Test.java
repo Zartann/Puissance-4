54028748d9f0715704a34fb8bed3fb205f623e25
alexisld <alexisledantec@gmail.com>
2014-04-21 00:25:41 +0200

    Ajout d'une classe pour faire tous nos tests

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Boolean nu = null;
+		Boolean pos = new Boolean(true);
+		Boolean neg = new Boolean(false);
+		
+		System.out.println(pos);
+	}
+
+}

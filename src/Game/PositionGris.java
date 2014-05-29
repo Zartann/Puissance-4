0dfee613c61a85f22013dbb6b6fcb86562eb91c5
alexisld <alexisledantec@gmail.com>
2014-05-29 22:17:18 +0200

    Correction d'erreur et amélioration de PositionGris

--- src/Game/PositionGris.java
+++ src/Game/PositionGris.java
@@ -2,7 +2,7 @@ package Game;
 
 public class PositionGris extends Position{
 	long gris;
-	
+
 	/**
 	 * 
 	 * @param playerPos
@@ -11,7 +11,7 @@ public class PositionGris extends Position{
 	 * @param maxHeight
 	 * Les positions inutiles du plateau sont déterminées (et instanciées dans gris) directement dans ce constructeur
 	 */
-	
+
 	public PositionGris(long playerPos, long advPos, int maxWidth, int maxHeight) {
 		super(playerPos, advPos, maxWidth, maxHeight);
 		//Pour chaque case, on regarde si elle est occupée et si elle peut être étendue en solution dans chaque direction
@@ -31,9 +31,9 @@ public class PositionGris extends Position{
 				}
 				if ()
 			}
-			
+
 		}*/
-		
+
 		//On remplit le tableau de jetons blancs/noirs puis on grise les jetons ne donnant pas d'alignements de 4
 		long player2 = playerPos, adv2 = advPos;
 		long playerToutSeul = 0;
@@ -45,7 +45,8 @@ public class PositionGris extends Position{
 				advToutSeul = placeIemeBit (advToutSeul, i, (byte)1);
 			}
 		}*/
-		for (int i=0;i<maxWidth;i++){
+		/*Faux, car remplit à l'envers, également trop couteux
+		   for (int i=0;i<maxWidth;i++){
 			for (int j=0;j<maxHeight;j++){
 				if ((adv2 % 2 ==0)||(player2 %2 ==1))playerToutSeul+=1;
 				if ((player2 % 2 ==0)||(adv2 % 2 ==1)) advToutSeul+=1;
@@ -56,7 +57,30 @@ public class PositionGris extends Position{
 			}
 			playerToutSeul <<= 1;
 			advToutSeul <<=1;
+		}*/
+
+		//plateauPlein correspond à un plateau rempli, ligne du dessus comprise.
+		long plateauPlein = ((long) 1) << (maxWidth * (maxHeight+1));
+		plateauPlein--;
+
+		//PlayerToutSeul et advToutSeul vont contenir le plateau en entier avec que des 1 sauf aux 
+		//positions des jetons de Adversary et de Player respectivement
+		playerToutSeul = advPos ^ plateauPlein;
+		advToutSeul = playerPos ^ plateauPlein;
+
+		//ligneSup correspond à la seule ligne du dessus remplie avec des 1.
+		long ligneSup = 0;
+		for(int i = 0; i < maxWidth; i++){
+			ligneSup++;
+			ligneSup <<= maxHeight+1;
 		}
+		ligneSup >>= 1;
+
+		//On élimine la ligne du dessus, actuellement remplie de 0
+		playerToutSeul ^= ligneSup;
+		advToutSeul ^= ligneSup;
+
+
 		//On trouve les alignements ainsi créés
 		long columnPlayer, linePlayer, diagPlayer, antidiagPlayer;
 		long columnAdv, lineAdv, diagAdv, antidiagAdv;
@@ -72,7 +96,7 @@ public class PositionGris extends Position{
 
 		antidiagPlayer = (playerPos & (playerPos >> maxHeight+2) & (playerPos >> 2*maxHeight+4) & (playerPos >> 3*maxHeight+6));
 		antidiagAdv = (advPos & (advPos >> maxHeight+2) & (advPos >> 2*maxHeight+4) & (advPos >> 3*maxHeight+6));
-		
+
 		//On détermine tous les jetons susceptibles de former une séquence de 4 dans chaque direction pour chaque joueur
 		//(y compris les jetons virtuels)
 		columnPlayer = (columnPlayer | (columnPlayer << 1) | (columnPlayer << 2) | (columnPlayer << 3));
@@ -86,30 +110,30 @@ public class PositionGris extends Position{
 
 		antidiagPlayer = (antidiagPlayer | (antidiagPlayer << maxHeight+2) | (antidiagPlayer << 2*maxHeight+4) | (antidiagPlayer << 3*maxHeight+6));
 		antidiagAdv = (antidiagAdv | (antidiagAdv << maxHeight+2) | (antidiagAdv << 2*maxHeight+4) | (antidiagAdv << 3*maxHeight+6));
-		
+
 		//On s'abstrait de la direction des séquences
 		long playerVirtualUtil = columnPlayer | linePlayer | diagPlayer | antidiagPlayer ;
 		long advVirtualUtil = columnAdv | lineAdv | diagAdv | antidiagAdv;
-		
+
 		//Le "et" a lieu avec player/adv pour ne conserver que les jetons existants utiles pour le joueur en question
 		long playerUtil = playerVirtualUtil & playerPos;
 		long advUtil = advVirtualUtil & advPos;
-		
+
 		//Les jetons gris sont les inutiles pour les deux joueurs
 		gris = (playerPos ^ playerUtil) | (advPos ^ advUtil);
-		
+
 		this.playerPos=playerUtil;
 		this.advPos=advUtil;
-	/*for (int i =0;i<maxHeight*maxWidth;i++){
-		//On cherche si cette case est occupée par l'un des joueurs
-		if (trouveIemeBit(playerPos, i)==1){
-			//On trouve si un alignement de 4 contenant ce jeton existe
-			long column = 
-		}
-		else if (trouveIemeBit(advPos, i)==1){
-			
-		}
-		else {}
-	}*/
+		/*for (int i =0;i<maxHeight*maxWidth;i++){
+			//On cherche si cette case est occupée par l'un des joueurs
+			if (trouveIemeBit(playerPos, i)==1){
+				//On trouve si un alignement de 4 contenant ce jeton existe
+				long column = 
+			}
+			else if (trouveIemeBit(advPos, i)==1){
+		
+			}
+			else {}
+		}*/
 	}
 }

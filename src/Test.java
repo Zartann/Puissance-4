ae43e74739ece3b66e0217fa0ff8f31c3e8a8ef2
alexisld <alexisledantec@gmail.com>
2014-05-29 18:46:43 +0200

    Petit test

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,12 @@
+import java.io.FileNotFoundException;
+
+import Game.PlateauCourant;
+import Game.Position;
+import Search.AlphaBeta;
+import Search.MiniMaxElague;
+import Search.NegaMaxElague;
+import Search.StateValue;
+
 
 public class Test {
 
@@ -5,12 +14,22 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Boolean nu = null;
-		Boolean pos = new Boolean(true);
-		Boolean neg = new Boolean(false);
+		String path ="resources/boards/ended.cfg";
 		
-		System.out.println(pos);
+		PlateauCourant board;
+		try {
+			board = Main.importFromFile(path);
+		
+			System.out.println(board);
+			
+			Position sym = board.cle().symmetricPosition();
+			board.importFromLong(sym.playerPos, sym.advPos);
+			System.out.println(board);
+			
+		} catch (FileNotFoundException e) {
+
+			System.out.println("Le fichier est introuvable !");
+		}
 	}
 
 }

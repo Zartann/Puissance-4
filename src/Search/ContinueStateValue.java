e84d474f427d806f0bc380a1a1d700df0e58a97f
Thibaut Courvoisier <thibaut.courvoisier@gmail.com>
2014-05-30 23:54:37 +0200

    Création de méthodes pour ContinueStateValue analogues à celles de StateValue

--- src/Search/ContinueStateValue.java
+++ src/Search/ContinueStateValue.java
@@ -12,5 +12,82 @@ public class ContinueStateValue {
 	public ContinueStateValue (StateValue valDiscrete){
 		this.valDiscrete = valDiscrete;
 	}
+	
+	public boolean isLoss(){
+		return (valDiscrete == StateValue.LOSS);
+	}
 
+	public boolean isNotWinNorLoss(){
+		return ((valDiscrete != StateValue.LOSS)&&(valDiscrete != StateValue.WIN));
+	}
+
+	public boolean isWin(){
+		return (valDiscrete == StateValue.WIN);
+	}
+	
+	/**
+	 * Indique si la valeur est supérieure ou égale à s
+	 * @param s
+	 * @return boolean
+	 */
+	public boolean betterOrEquals(ContinueStateValue s){
+
+		//Beaucoup plus rapide qu'avec ne serait-ce qu'un seul if !!!
+		return (s.valDiscrete==StateValue.LOSS)||
+				(this.valDiscrete==StateValue.WIN)||
+				((this.valDiscrete!=null) &&(this.valDiscrete==s.valDiscrete))||
+				(this.valDiscrete ==null)&&
+				(((s.valDiscrete==null)&&(this.valContinue>=s.valContinue))||
+						((s.valDiscrete==StateValue.DRAW)&&(this.valContinue>=0)));
+	}
+	
+	/**
+	 * Indique si la valeur est inférieure ou égale à s
+	 * @param s
+	 * @return boolean
+	 */
+	public boolean lessOrEquals(ContinueStateValue s){
+		
+		return s.betterOrEquals(this);
+	}
+
+	/**
+	 * 
+	 * @param s
+	 * @return Etat maximal entre this et s
+	 */
+	public ContinueStateValue max(ContinueStateValue s){
+		if(this.betterOrEquals(s))
+			return this;
+
+		return s;
+	}
+
+	/**
+	 * 
+	 * @param s
+	 * @return Etat minimal entre this et s
+	 */
+	public ContinueStateValue min(ContinueStateValue s){
+		if (this.lessOrEquals(s))
+			return this;
+		return s;
+	}
+	
+	public ContinueStateValue opposite(){
+		if (this.valDiscrete!=null)
+			return new ContinueStateValue (this.valDiscrete.opposite());
+		else return new ContinueStateValue (-this.valContinue);
+	}
+
+	public String toString(){
+		if(this.valDiscrete!=null){
+			switch (this.valDiscrete){
+			case DRAW : return "DRAW";
+			case WIN : return "WIN";
+			default : return "LOSS";
+			}
+		}
+		else return "Pas eu le temps de déterminer";
+	}
 }

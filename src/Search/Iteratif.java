541d1efe4a157dcfc03a1da3dffa102ca9559675
Thibaut Courvoisier <thibaut.courvoisier@gmail.com>
2014-06-02 23:09:39 +0200

    Commentaire dans Iteratif

--- src/Search/Iteratif.java
+++ src/Search/Iteratif.java
@@ -8,11 +8,13 @@ import Game.Position;
 import Game.PositionGris;
 
 public class Iteratif {
-
+	/**
+	 * On ne stockera aucune entrée correspondant à une profondeur supérieure
+	 */
 	int profondeurStockageMax;
 	
 	/**
-	 * Contient le nombre total de positions qui ont été évaluées.
+	 * Contient le nombre total de positions qui ont été évaluées
 	 */
 	public int totalPositions = 0;
 
@@ -27,110 +29,11 @@ public class Iteratif {
 		profondeurStockageMax = prof;
 	}
 	
-	/*public StateValue iteratif(PlateauCourant state){
-		
-		StateValue value = StateValue.DRAW;
-		for(int i = 0; i <= state.nombreCoupsRestants(); i += 2){
-			value = iteration(state, StateValue.LOSS, StateValue.WIN, 0, i);
-			if(!value.isDraw())
-				break;
-			
-			//On change d'itération, donc on change de table de hachage.
-			formerHashTable = currentHashTable;
-			currentHashTable = new HashMap<Position, StateValueWithBound>();
-		}
-		
-		return value;
-	}
-	
-	public StateValue iteration(PlateauCourant state, StateValue alpha, StateValue beta,
-			int profondeur, int profondeurMaxIteration){
-		totalPositions++;
-		Position pos = state.cle();
-		
-		//Evaluation rapide de l'état si possible
-		
-		List<Integer> shots = state.validShots();
-
-		StateValue value = state.result();
-		
-		//Si c'est au tour de l'adversaire, on inverse le résultat calculé
-		if(!state.playerIsNext())
-			value = value.opposite();
-
-		//On s'arrête si aucun coup n'est possible ou si l'issue est décidée
-		if(shots.isEmpty() || !value.isDraw()){
-			//L'évaluation est accurate
-			if(profondeur <= profondeurStockageMax)
-				currentHashTable.put(pos, new StateValueWithBound(value, 0));
-			
-			return value;
-		}
-		
-		
-		//Evaluation de l'état si profondeur de recherche atteinte
-		if(profondeur == profondeurMaxIteration)
-			return state.eval();
-		
-		//Verification si l'état a été complètement évalué à l'itération précédente
-		StateValueWithBound formerValue = formerHashTable.get(pos);
-		if(formerValue != null && !formerValue.value.isDraw()){
-			if(profondeur <= profondeurStockageMax)
-				currentHashTable.put(pos, new StateValueWithBound(formerValue.value, 0));
-		}
-		
-		//Comparaison avec la table de hachage
-		
-		StateValueWithBound boundValue = currentHashTable.get(pos);
-		StateValue newAlpha = alpha, newBeta = beta;
-		
-		if(boundValue != null){
-			if(boundValue.isLowerBound())
-				newAlpha = newAlpha.max(boundValue.value);
-			
-			else if(boundValue.isUpperBound())
-				newBeta = newBeta.min(boundValue.value);
-			
-			else{
-				newAlpha = boundValue.value;
-				newBeta = boundValue.value;
-			}
-			
-			if(newAlpha.betterOrEquals(newBeta))
-				return boundValue.value;
-		}
-		
-		//On initialise l'état à LOSS
-		value = StateValue.LOSS;
-
-		StateValue score;
-		for(int shot : shots){
-			state.playNext(shot);
-			
-			//On récupère l'opposé du coup suivant et on garde le max avec la valeur courante.
-			score = iteration(state, newBeta.opposite(), newAlpha.opposite(), profondeur+1, profondeurMaxIteration).opposite();
-			value = value.max(score);
-			newAlpha = newAlpha.max(score);
-			
-			state.undoLast();
-
-			if(score.betterOrEquals(beta))
-				break;
-			
-		}
-
-		if(profondeur <= profondeurStockageMax)
-			currentHashTable.put(pos, new StateValueWithBound(value, newAlpha, newBeta));
-			
-		return value;
-		
-	}*/
-	
-
 	public StateValue iteratif2(PlateauCourant state){
 		
 		StateValue value = StateValue.DRAW;
 		for(int i = 0; i <= state.nombreCoupsRestants(); i += 2){
+			//On effectue un parcours en largeur d'abord jusqu'à la profondeur i, incrémentée de 2 à chaque étape
 			value = iteration2(state, StateValue.LOSS, StateValue.WIN, 0, i);
 			if(!value.isDraw())
 				break;
@@ -142,16 +45,19 @@ public class Iteratif {
 			StateValueWithBound v;
 			for(Position pos : formerHashTable.keySet()){
 				v = formerHashTable.get(pos);
+				//Les valeurs v (donc v.value vu les constructeurs) ne sont jamais nulles avec ce type de remplacement
 				if(!v.value.isDraw()){
+					//Toute position non DRAW est accurate
 					v.setBound(0);
 					currentHashTable.put(pos, v);
 				}
 			}
 		}
-		
+		//On retrouve la valeur trouvée après exploration jusqu'à la fin de la partie
 		return value;
 	}
 	
+	//Fonction récursive appelée par Iteratif2
 	public StateValue iteration2(PlateauCourant state, StateValue alpha, StateValue beta,
 			int profondeur, int profondeurMaxIteration){
 		totalPositions++;
@@ -198,6 +104,7 @@ public class Iteratif {
 				newBeta = boundValue.value;
 			}
 			
+			//Coupure éventuelle
 			if(newAlpha.betterOrEquals(newBeta))
 				return boundValue.value;
 		}
@@ -216,9 +123,11 @@ public class Iteratif {
 			
 			state.undoLast();
 			
+			//On ne fera pas mieux que WIN -> coupures
 			if (score.isWin())
 				break;
-
+			
+			//Coupure car l'adversaire n'acceptera pas un résultat supérieur à newBeta
 			if(score.betterOrEquals(newBeta))
 				break;
 			
@@ -235,6 +144,9 @@ public class Iteratif {
 		
 	}
 	
+	
+	//On ajoute maintenant la notion de jeton isolé (gris)
+	
 	//On utilise deux tables de hachage, une pour l'itération précédente, une pour l'actuelle
 		HashMap<PositionGris, StateValueWithBound> formerGrisHashTable = 
 				new HashMap<PositionGris, StateValueWithBound>();
@@ -246,7 +158,9 @@ public class Iteratif {
 		
 		StateValue value = StateValue.DRAW;
 		for(int i = 0; i <= state.nombreCoupsRestants(); i += 2){
+			//Parcours en largeur d'abord
 			value = iterationGris(state, StateValue.LOSS, StateValue.WIN, 0, i);
+			//Coupure possible grâce au principe de prudence
 			if(!value.isDraw())
 				break;
 			
@@ -257,13 +171,15 @@ public class Iteratif {
 			StateValueWithBound v;
 			for(PositionGris pos : formerGrisHashTable.keySet()){
 				v = formerGrisHashTable.get(pos);
+				//Les valeurs v (donc v.value vu les constructeurs) ne sont jamais nulles avec ce type de remplacement
 				if(!v.value.isDraw()){
+					//Valeur accurate dans ce cas
 					v.setBound(0);
 					currentGrisHashTable.put(pos, v);
 				}
 			}
 		}
-		
+		//On renvoie la valeur obtenue après parcours de toutes les parties possibles (avec élagage)
 		return value;
 	}
 	
@@ -271,7 +187,6 @@ public class Iteratif {
 			int profondeur, int profondeurMaxIteration){
 		totalPositions++;
 		PositionGris pos = state.cleGris();
-		//System.out.println(pos);
 		//Evaluation rapide de l'état si possible
 		
 		List<Integer> shots = state.validShots();
